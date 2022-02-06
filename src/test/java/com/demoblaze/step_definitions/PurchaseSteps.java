ac50ce6ede4b5424c229091a24ea62b6981728df
ibrahimgokalp <gokalp88@gmail.com>
2022-02-06 18:24:42 +0300

    installing utilities

---
+++ src/test/java/com/demoblaze/step_definitions/PurchaseSteps.java
@@ -0,0 +1,17 @@
+package com.demoblaze.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class PurchaseSteps {
+
+    @Given("User is on the home page")
+    public void user_is_on_the_home_page() {
+
+    }
+
+    @When("User adds {string} from {string}")
+    public void user_adds_from(String product, String category) {
+
+    }
+}
